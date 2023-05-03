afcab850d2bdfdd701f1fe26510cc66112769618
DhineshSingha <131573396+DhineshSingha@users.noreply.github.com>
2023-05-03 18:55:25 +0530

    Delete day1 directory

--- day1/Student.java
+++
@@ -1,9 +0,0 @@
-package week1.day1;
-public class Student
-{
-	String studentName = "Dhinesh";
-	long rollNo = 871527091306l;
-	String collegeName = "MGR University";
-	int markScored = 482;
-	float cgpa=96.4f;
-}
