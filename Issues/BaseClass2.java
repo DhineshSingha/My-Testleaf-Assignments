d0fa265c88ac2afbe7b285d8943ba7577fcab847
DhineshSingha <131573396+DhineshSingha@users.noreply.github.com>
2023-05-21 19:07:30 +0530

    Add files via upload

---
+++ Issues/BaseClass2.java
@@ -0,0 +1,29 @@
+package week5.day2;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+public class BaseClass2
+{
+	public ChromeDriver driver;
+	@BeforeMethod
+	public void precondition()
+	{
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.partialLinkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+	}
+	
+	@AfterMethod
+	public void postcondition()
+	{
+		driver.close();
+	}
+}
---
+++ Issues/LearnInvocationTimeout.java
@@ -0,0 +1,22 @@
+package week5.day2;
+import java.time.Duration;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+public class LearnInvocationTimeout extends BaseClass2
+{
+	@Test (invocationCount = 2, invocationTimeOut = 50000)
+	public void runCreateLead() 
+	{
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Hari");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("R");
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys("99");
+		driver.findElement(By.name("submitButton")).click();
+	}
+}
